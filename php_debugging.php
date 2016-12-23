8bef411ec1b83e946053231c065adfada5ed6ead
Tim Zwart <timwaagh@gmail.com>
2016-12-23 10:20:24 +0100

    yay custom vardump works as expected

--- php_debugging.php
+++ php_debugging.php
@@ -1,29 +1,43 @@
 <?php
     function var_tim($var, $max_depth = 3, $current_depth = 1){
-        $looper = function($var) use ($max_depth, $current_depth) {
+        $spacer = function() use ($max_depth, $current_depth) {
+            for($i = 1; $i < $current_depth ; $i++)
+                echo "    ";
+        };
+        $looper = function($var) use ($max_depth, $current_depth, $spacer) {
             foreach($var as $key => $val) { 
+               $spacer();
                echo "$key => ";
                var_tim($val, $max_depth, $current_depth+1);
                echo "\n";
             }
         };
         if($current_depth > $max_depth) {
-            return '..';
+            echo '..';
+            return;
         }
-        if(gettype($var) == "object") {
+        $type = gettype($var);
+        if($type == "object") {
             $var_as_array = get_object_vars($var); 
-            echo "{ ";
+            echo "{ \n";
             $looper($var_as_array);
-            echo "} ";
+            $spacer();
+            echo "} \n";
         }
-        else if(gettype($var) == "array") {
-           echo " [";
+        else if($type == "array") {
+           echo " [\n";
            $looper($var);
-           echo " [";
-
+           $spacer();
+           echo " ]\n";
         }
         else {
-            var_dump($var);
+            if (is_null($var)) {
+                echo "NULL";
+            }
+            else {
+                echo "$type: ";
+                echo "$var";
+            }
         }
     }
 ?>
